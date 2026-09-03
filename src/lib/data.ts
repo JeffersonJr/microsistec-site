cde15b50f9e903f49949e3705f47ccb72b9d10fe
Jefferson Campos <contato@jeffersonjunior.com.br>
2026-09-03 14:04:40 -0300

    feat: Adiciona logo Epungo

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -912,6 +912,7 @@ export const portalIntegrations: PortalIntegration[] = [
     logoBg: "bg-rose-400/10",
     logoTextColor: "text-rose-600",
     logoText: "EP",
+    logoImg: "https://www.google.com/s2/favicons?domain=epungo.com.br&sz=128",
     officialColor: "#FB7185",
   },
   {
