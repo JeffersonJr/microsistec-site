58f50672a597815f5e9852d012b609672467ce86
Jefferson Campos <contato@jeffersonjunior.com.br>
2026-09-03 16:39:43 -0300

    perf: Troca a última imagem grande do AlugarJá para a API do Google Favicons

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -438,7 +438,7 @@ export const portalIntegrations: PortalIntegration[] = [
     logoBg: "bg-indigo-500/10",
     logoTextColor: "text-indigo-600",
     logoText: "AJ",
-    logoImg: "https://imob.online/imgs/portais/alugarja.png",
+    logoImg: "https://www.google.com/s2/favicons?domain=alugarja.com.br&sz=64",
     officialColor: "#6366F1",
   },
   {
